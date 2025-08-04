1a145e579da8938fc80f3821de56798b6ef6e7a7
arkadashiim <arkadashiim@mail.ru>
2025-08-04 16:45:34 +0300

    feat: interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideZoneChangeDetection,
+} from "@angular/core";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideRouter } from "@angular/router";
+import { IEnvironment } from "@tecya/interfaces";
+import { environment } from "../env/environment";
+import { routes } from "./app.routes";
+import { HttpErrorInterceptor } from "./shared/error";
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+export const ENVIRONMENT = new InjectionToken<IEnvironment>("ENVIRONMENT");
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync("noop"),
+    provideHttpClient(withInterceptors([HttpErrorInterceptor])),
+    {
+      provide: ENVIRONMENT,
+      useValue: environment,
+    },
+  ],
 };
